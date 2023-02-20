240f0d89ace46c8d619379d515b93c55a0fb5d58
Кирилл <105377400+Cheburek254@users.noreply.github.com>
2023-02-20 11:14:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,23 @@ from main_app import *
 from train import *
 from runner import *
 from kivy.animation import Animation
+from kivy.lang.builder import Builder
+from kivy.uix.scrollview import ScrollView
+from texst import *
 
-color = (0.43, 0.4, 0.84, 1)
-btn_color = (0.18, 0.07, 0.83, 1)
+
+KV = '''
+Screen:
+
+    MDRoundFlatButton:
+       text: "MDROUNDFLATBUTTON"
+'''
+
+
+    
+
+color = (0.27, 0.27, 0.27, 1)
+btn_color = (0.79, 0.6, 0, 1)#(0.41, 0.49, 1, 1)
 a = (0,0,0,1)
 b = (0.65,0.54,0,1)
 c = (0.43, 0.4, 0.84, 1)
@@ -28,20 +42,56 @@ a_btn = (0.18,0.18,0.18,1)
 b_btn = (0.38,0.32,0,1)
 c_btn = (0.18, 0.07, 0.83, 1)
 d_btn = (0.07,0.3,0.08,1)
+black = (0, 0, 0, 1)
 
-class First(Screen):
+class Policy(Screen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        instr = Label(text = ti)
-        self.btn = Button(text ='[u]' +  'далее'+ '[/u]', size_hint=(0.3, 0.2), pos_hint = {'center_x': 0.5}, background_color = btn_color, markup=True)
+        s = ScrollView(size_hint=(1,1))
+        
+        self.label = Label(text = sogl,halign = 'right',size_hint_y = None)
+        
+        '''w = label.texture_size[0]
+        h = label.texture_size[1]
+        label.height = h'''
+        s.add_widget(self.label)
+        self.label.bind(size = self.resize)
+        self.btn = Button(text ='[b]' +  'Принять'+ '[/b]', size_hint=(0.5, 0.4), pos_hint = {'center_x': 0.5}, markup=True,color=black,background_normal = 'button_horizontal.png',font_size = 90)
         self.btn.on_press = self.next
         outer = BoxLayout(orientation = 'vertical', padding = 8, spacing = 8)
-        outer.add_widget(instr)
+        outer.add_widget(s)
         outer.add_widget(self.btn)
         self.add_widget(outer)
     def next(self):
         
+        self.manager.current = 'first'
+    def resize(self,w,h):
+        self.label.text_size = (self.label.width,None)
+        self.label.texture_update()
+        self.label.height = self.label.texture_size[1]
+
+
+
+class First(Screen):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text = ti,font_size = 45,halign = 'justify')
+        self.btn = Button(text ='[b]' +  'далее'+ '[/b]', size_hint=(0.5, 0.4), pos_hint = {'center_x': 0.5}, markup=True,color=black,background_normal = 'button_horizontal.png',font_size = 90)
+        self.btn.on_press = self.next
+        outer = BoxLayout(orientation = 'vertical', padding = 8, spacing = 8)
+        self.btn3 = Button(text ='[b]' +  'Пользовательское\nсоглашение'+ '[/b]', size_hint=(0.5, 0.4), pos_hint = {'center_x': 0.5}, markup=True,color=black,background_normal = 'button_horizontal.png',font_size = 50)
+        self.btn3.on_press = self.next3
+        l1 = BoxLayout()
+        outer.add_widget(instr)
+        l1.add_widget(self.btn)
+        l1.add_widget(self.btn3)
+        outer.add_widget(l1)
+        self.add_widget(outer)
+    def next(self):
+        
         self.manager.current = 'main'
+    def next3(self):
+        self.manager.current = 'policy'
         
 
 class Main(Screen):
@@ -50,13 +100,13 @@ class Main(Screen):
         instr = Label(text = '[b]'+'KivyHealth',halign = 'right',markup = True,font_size = 90)
         '''dropdown = DropDown()
         mainbutton = Button(text='Меню',  size_hint=(0.7, 0.6), pos_hint = {'center_x': 0.5})'''
-        self.btn1 = Button(text ='[u]' +  'Тест Руфье'+ '[/u]', size_hint=(0.7, 0.6), pos_hint = {'center_x': 0.5}, background_color = btn_color, markup=True)
+        self.btn1 = Button(text ='[b]' +  'Тест\nРуфье'+ '[/b]', size_hint=(0.8, 0.7), pos_hint = {'center_x': 0.5}, markup=True,color=black,background_normal = 'button_vertical.png',font_size = 60)
         self.btn1.on_press = self.next1
-        self.btn2 = Button(text ='[u]' +  'Тренировки'+ '[/u]', size_hint=(0.7, 0.6), pos_hint = {'center_x': 0.5}, background_color = btn_color, markup=True)
+        self.btn2 = Button(text ='[b]' +  'Тренировки'+ '[/b]', size_hint=(0.8, 0.7), pos_hint = {'center_x': 0.5},markup=True,color=black,background_normal = 'button_vertical.png',font_size = 50)
         self.btn2.on_press = self.next2
-        self.btn3 = Button(text ='[u]' +  'Настройки'+ '[/u]', size_hint=(0.7, 0.6), pos_hint = {'center_x': 0.5}, background_color = btn_color, markup=True)
-        self.btn3.on_press = self.next3
-        wimg = Image(source='main.png')
+        '''self.btn3 = Button(text ='[b]' +  'Пользовательское\nсоглашение'+ '[/b]', size_hint=(0.8, 0.7), pos_hint = {'center_x': 0.5}, markup=True,color=black,background_normal = 'button_vertical.png',font_size = 50)
+        self.btn3.on_press = self.next3'''
+        wimg = Image(source='main_icon.png')
         line1 = BoxLayout( padding = 8, spacing = 8)
         line2= BoxLayout(orientation='vertical')
         #line1.add_widget(line2)
@@ -65,7 +115,7 @@ class Main(Screen):
         outer = BoxLayout(orientation = 'vertical', padding = 8, spacing = 8)
         outer.add_widget(self.btn1)
         outer.add_widget(self.btn2)
-        outer.add_widget(self.btn3)
+        #outer.add_widget(self.btn3)
         line1.add_widget(line2)
         line1.add_widget(outer)
         
@@ -85,10 +135,11 @@ class Main(Screen):
     def next2(self):
         self.manager.current = 'train_main'
     def next3(self):
-        self.manager.current = 'settings'
+        self.manager.current = 'policy'
+        
 
 
-
+'''
 class Settings(Screen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
@@ -157,14 +208,17 @@ class Settings(Screen):
         Window.clearcolor = color
         self.manager.current = 'main'
 
-
+'''
 
 class KivyHealth(App):
     def build(self):
         sm = ScreenManager()
+        
+            
         sm.add_widget(First(name='first'))
         sm.add_widget(Main(name='main'))
-        
+        sm.add_widget(Policy(name='policy'))
+
         sm.add_widget(train_main(name='train_main'))
         sm.add_widget(tr_l1_1(name='tr_l1_1'))
         sm.add_widget(tr_l1_2(name='tr_l1_2'))
@@ -184,7 +238,7 @@ class KivyHealth(App):
         sm.add_widget(tr_l3_4(name='tr_l3_4'))
         sm.add_widget(tr_l3_5(name='tr_l3_5'))
         
-        sm.add_widget(Settings(name='settings'))
+        #sm.add_widget(Settings(name='settings'))
         sm.add_widget(InstrScr(name='test'))
         sm.add_widget(PulseScr(name='pulse1'))
         sm.add_widget(CheckSits(name='sits'))
@@ -197,5 +251,4 @@ class KivyHealth(App):
 
 app = KivyHealth()
 app.run()
-
 
